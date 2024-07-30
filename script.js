b34c3356fa3eed9b349f8ed76333a8778ef01f8e
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-07-30 15:53:08 -0300

    Se implemento la llamada a la funcion para mostrar las partidas pasadas

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const createGameButtons = document.querySelectorAll('.create-game-button');
     const rematchButtons = document.querySelectorAll('.rematch-button');
     const gameContainer = document.querySelector('.game-container');
+    const gameList = document.querySelector('.game-list');
+    
+    if (gameList !== null) {
+        gameList.innerHTML = '';
+        showPartidas(gameList);
+    }
 
     if (gameContainer !== null) {
         const mode = localStorage.getItem('gameMode') || 'clasico';
