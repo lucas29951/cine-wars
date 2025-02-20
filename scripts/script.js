675001148d03dc784d01cad6983b4a0505cab840
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-02-20 17:37:11 -0300

    Configuracion del listener para la accion desplegable de las instrucciones

--- scripts/script.js
+++ scripts/script.js
@@ -36,3 +36,10 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+document.querySelectorAll(".accordion-header").forEach(button => {
+    button.addEventListener("click", () => {
+        const content = button.nextElementSibling;
+        content.style.display = content.style.display === "block" ? "none" : "block";
+    });
+});
